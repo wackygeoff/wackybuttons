71cdda516951fde135fe742cf0fb9d01be6aa374
wackygeoff <webmaster@wackybuttons.com>
2015-03-03 11:42:29 -0500

    update shopcart.js
    more edits to shopcart.js. fixed Wacky address. Fixed an unordered list
    in address line 2 help

--- js/shopcart.js
+++ js/shopcart.js
@@ -22,14 +22,14 @@ $('#dialog_content').html(inpcode);
 
 function pickuploc(){<br />
 $('#dialog_title').html("Pick up location");
-inpcode = 'Wacky Buttons, Inc.<br />101 Lincoln Parkway Suite E<br />East Rochester, NY 14445<br /> <br />';
+inpcode = 'Wacky Buttons, Inc.<br />101 Lincoln Parkway Suite A<br />East Rochester, NY 14445<br /> <br />';
 inpcode = inpcode + '<iframe width="425" height="350" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="http://maps.google.com/maps?f=q&amp;source=s_q&amp;hl=en&amp;geocode=&amp;q=Wacky+Buttons,+East+Rochester,+NY&amp;aq=1&amp;sll=37.0625,-95.677068&amp;sspn=38.638819,56.953125&amp;vpsrc=0&amp;ie=UTF8&amp;hq=Wacky+Buttons,&amp;hnear=East+Rochester,+Monroe,+New+York&amp;t=m&amp;ll=43.11617,-77.480596&amp;spn=0.016491,0.01518&amp;output=embed"></iframe><br /><small><a href="http://maps.google.com/maps?f=q&amp;source=embed&amp;hl=en&amp;geocode=&amp;q=Wacky+Buttons,+East+Rochester,+NY&amp;aq=1&amp;sll=37.0625,-95.677068&amp;sspn=38.638819,56.953125&amp;vpsrc=0&amp;ie=UTF8&amp;hq=Wacky+Buttons,&amp;hnear=East+Rochester,+Monroe,+New+York&amp;t=m&amp;ll=43.11617,-77.480596&amp;spn=0.016491,0.01518" style="color:#0000FF;text-align:left">View Larger Map</a></small>';
 $('#dialog_content').html(inpcode);
 }
 
 function streetaddyhelp(){
 $('#dialog_title').html("About Address (Line 2)");
-inpcode = 'You can enter your apartment number, suite number, floor number, etc... on this line.</li><li>You can also use this line if your address is just really long and you just need more room to write your address.</li><li>If you have more than 2 lines in your address, you can combine 2 lines onto one line by separateing them with a comma(,).</li><li>If you have any questions about inputting your shipping address please feel free to ask. We would be glad to help you out (Our phone number is 585-267-7670).';
+inpcode = '<ul><li>You can enter your apartment number, suite number, floor number, etc... on this line.</li><li>You can also use this line if your address is just really long and you just need more room to write your address.</li><li>If you have more than 2 lines in your address, you can combine 2 lines onto one line by separateing them with a comma(,).</li><li>If you have any questions about inputting your shipping address please feel free to ask. We would be glad to help you out (Our phone number is 585-267-7670).</li></ul>';
 $('#dialog_content').html(inpcode);
 }
 
